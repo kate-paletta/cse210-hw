78a076946d3fb9a08ed095e79d74f9b385eebaef
Kate Paletta <kate.f.knox@gmail.com>
2024-02-28 19:24:41 -0700

    step 1 and 2 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number?");
+        string answer = Console.ReadLine();
+        int magicNum = int.Parse(answer);
+
+        int guess = -1;
+
+        while (guess != magicNum)
+        {
+            Console.WriteLine("What is your guess?");
+            string answer2 = Console.ReadLine();
+            guess = int.Parse(answer2);
+
+            if (guess < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+        }
+        
     }
 }
