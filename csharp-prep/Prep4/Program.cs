020f2ef9d0a834c46bf2fb8f2503655fbcbc57c9
Kate Paletta <kate.f.knox@gmail.com>
2024-02-28 20:05:06 -0700

    step 1

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int num = -1;
+
+        while (num != 0)
+        {
+            Console.Write("Enter number: ");
+            num = int.Parse(Console.ReadLine());
+            numbers.Add(num);
+
+        }
+
+        //Get the total
+        int total = 0;
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+        Console.WriteLine($"The sum is: {total}");
+
+        //Get the average
+        
     }
 }
