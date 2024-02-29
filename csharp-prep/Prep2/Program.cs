90a76c311e70b204c39b17870ce2b1eaa24048fe
Kate Paletta <kate.f.knox@gmail.com>
2024-02-28 18:10:46 -0700

    prep 2 step 1 -2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int gradePerc = int.Parse(userInput);
+
+        if (gradePerc >= 90)
+        {
+            Console.WriteLine("You got an A");
+        }
+        else if (gradePerc >= 80)
+        {
+            Console.WriteLine("You got a B");
+        }
+        else if (gradePerc >= 70)
+        {
+            Console.WriteLine("You got a C");
+        }
+        else if (gradePerc >= 60)
+        {
+            Console.WriteLine("You got a D");
+        }
+        else
+        {
+            Console.WriteLine("You got an F");
+        }
+
+        if (gradePerc >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass, but keep working hard!");
+        }
     }
 }
