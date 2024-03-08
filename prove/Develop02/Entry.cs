3afb4b811f0a892aeb62a0736a5c42baaf0588a4
Kate Paletta <kate.f.knox@gmail.com>
2024-03-08 11:00:24 -0700

    prepped the classes with variables and method structures

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,11 @@
+public class Entry
+{
+    public string _date;
+    public string _promptTxt;
+    public string _entryTxt;
+
+    public void Display()
+    {
+        
+    }
+}
---
+++ prove/Develop02/PromptGen.cs
@@ -0,0 +1,9 @@
+public class PromptGen
+{
+    public List<string> _prompts;
+
+    public string GetRandomPrompt()
+    {
+        return " ";
+    }
+}
