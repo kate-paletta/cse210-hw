37306fc8b379fbe96f67f35328ebefe9e648d62c
Kate Paletta <kate.f.knox@gmail.com>
2024-03-08 14:40:21 -0700

    got the prompt generator class to work

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 public class Journal
 {
     public List<Entry> _entries;
--- prove/Develop02/PromptGen.cs
+++ prove/Develop02/PromptGen.cs
@@ -1,9 +1,25 @@
+using System;
+using System.Collections.Generic;
 public class PromptGen
 {
-    public List<string> _prompts;
-
-    public string GetRandomPrompt()
+    public List<string> _prompts = new List<string>
     {
-        return " ";
+        "What was something fun that you did today?",
+        "What is your goal for this week?",
+        "Did you talk to anyone interesting today?",
+        "What is one of your dream vacations?",
+        "What is something that you did that was better than yesterday?",
+        "What is something you are looking forward to?",
+        "Did you do any of your hobbies today?",
+        "What is something the Spirit taught you this week?",
+        "Did you have a scripture that stuck out to you this week of Come Follow Me?",
+        "What are you grateful for?"
+    };
+
+    public void GetRandomPrompt()
+    {
+        Random random = new Random();
+        int _promptPick = random.Next(_prompts.Count);
+        Console.WriteLine(_prompts[_promptPick]);
     }
 }
